cbff91f5fbde6d0dfd5cae05bb5dd5560bf4a91b
vidra74 <fjancic@inet.hr>
2017-11-16 22:56:14 +0100

    Initial commit

---
+++ app/src/main/java/bridge/franojancic/com/teamgame/MainActivity.java
@@ -0,0 +1,74 @@
+package bridge.franojancic.com.teamgame;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    public String mThreadPoruka;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DoTask().execute();
+                Snackbar.make(view, "Started thread and get message " + mThreadPoruka, Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class DoTask extends AsyncTask<Void, Void, String>{
+        @Override
+        protected String doInBackground(Void... pVoids) {
+            try{
+                Log.i(TAG, "DoTask.doInBackground");
+            } catch (Exception e) {
+                Log.e(TAG, "DoTask.doInBackgroung exception catched", e);
+            }
+            return "DoTask.doInBackground finished";
+        }
+
+        @Override
+        protected void onPostExecute(String pS) {
+            mThreadPoruka = pS;
+        }
+    }
+}
