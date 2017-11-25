83e8c5a86a318d77993603ee87dde0b9c2da5a88
vidra74 <fjancic@inet.hr>
2017-11-25 20:15:01 +0100

    Displaying messages, connection fixed.

--- app/src/main/java/bridge/franojancic/com/teamgame/MainActivity.java
+++ app/src/main/java/bridge/franojancic/com/teamgame/MainActivity.java
@@ -168,7 +168,7 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "setupChat()");
 
         // Initialize the array adapter for the conversation thread
-        mConversationArrayAdapter = new ArrayAdapter<String>(this, R.layout.content_main);
+        mConversationArrayAdapter = new ArrayAdapter<String>(this, R.layout.message);
 
         mConversationView.setAdapter(mConversationArrayAdapter);
 
@@ -308,6 +308,44 @@ public class MainActivity extends AppCompatActivity {
                             Toast.LENGTH_SHORT).show();
                     finish();
                 }
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // If BT is not on, request that it be enabled.
+        // setupChat() will then be called during onActivityResult
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            // Otherwise, setup the chat session
+        } else if (mChatService == null) {
+            setupChat();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mChatService != null) {
+            mChatService.stop();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Performing this check in onResume() covers the case in which BT was
+        // not enabled during onStart(), so we were paused to enable it...
+        // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+        if (mChatService != null) {
+            // Only if the state is STATE_NONE, do we know that we haven't started already
+            if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
+                // Start the Bluetooth chat services
+                mChatService.start();
+            }
         }
     }
 
